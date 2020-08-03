a8d7d05b3f8b5bf262924fcc8ce4d4b9cd9986d7
James Rowe <jamesrowe@outlook.com>
2020-08-03 19:57:22 -0400

    repo for help

--- employeeTrackerDBConnection.js
+++ employeeTrackerDBConnection.js
@@ -1,8 +1,9 @@
 const mysql = require("mysql");
 const fs = require("fs");
+const cTable = require("console.table");
 
 const password = fs.readFileSync("./password.config", "utf8");
-console.log(password);
+
 const connection = mysql.createConnection({
   host: "localhost",
 
@@ -17,5 +18,12 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadID);
-  connection.end();
+  connection.query(
+    "SELECT * FROM employees INNER JOIN roles ON employees.role_id=roles.id INNER JOIN departments ON roles.department_id",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      connection.end();
+    }
+  );
 });
